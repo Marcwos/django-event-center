c96102a0123a43c9deb56fecdd5bb6ecbf746b53
Jeremy Delgado <ubituna22@gmail.com>
2024-12-29 15:42:27 -0500

    Ultimos cambios aplicados

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib import messages
-from django.core.mail import send_mail, EmailMultiAl
+from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views import View
